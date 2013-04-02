f047e37bd373630db8f8c59beb15b73f871195cd
westernarc <deepspace30@gmail.com>
2013-04-02 17:33:56 -0400

    AGDG Bootcamp project: Garden Warden

---
+++ gardenwarden/src/com/westernarc/gardenwarden/Node/Node.java
@@ -0,0 +1,14 @@
+package com.westernarc.gardenwarden.Node;
+
+import com.badlogic.gdx.graphics.g3d.model.still.StillModel;
+
+public class Node {
+	protected StillModel model;
+	
+	public StillModel getModel() {
+		return model;
+	}
+	public void setModel(StillModel model) {
+		this.model = model;
+	}
+}
